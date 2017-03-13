e418761667b03ef05529009eef6c420558d5f769
Alexander Gurinov <Alexander Gurinov>
2017-03-13 20:36:55 +0200

    Add project files.

---
+++ Dyrix/Dynamics.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace Dyrix
+{
+    public sealed class Dynamics : IDisposable
+    {
+        private readonly HttpClient _httpClient;
+
+        private Dynamics(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public static async Task<Dynamics> CreateAsync(string resource, string directoryId, string clientId, string clientSecret)
+        {
+            var authenticationContext = new AuthenticationContext($"https://login.windows.net/{directoryId}");
+            var authenticationResult = await authenticationContext.AcquireTokenAsync(resource, new ClientCredential(clientId, clientSecret));
+
+            var httpClient = new HttpClient
+            {
+                Timeout = TimeSpan.FromMinutes(2),
+                BaseAddress = new Uri($"{resource}/api/data/v8.2/")
+            };
+
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authenticationResult.AccessToken);
+
+            return new Dynamics(httpClient);
+        }
+
+        public async Task<string> GetStringAsync(string request)
+        {
+            return await _httpClient.GetStringAsync(request);
+        }
+
+        public void Dispose()
+        {
+            _httpClient?.Dispose();
+        }
+    }
+}
---
+++ SampleConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dyrix;
+
+namespace SampleConsoleApp
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            RunAsync().Wait();
+        }
+
+        private static async Task RunAsync()
+        {
+            const string resource = "https://g666.crm4.dynamics.com";
+            const string directoryId = "71b07708-1af0-453c-9098-24ac683a143e";
+            const string clientId = "6f670244-2d51-40e1-837d-567dca629e66";
+            const string clientSecret = "e/O1vKNELlWSEXFClo8LaAiwzvrabrM07hEjfN0Cz9Q=";
+            
+            using (var dynamics = await Dynamics.CreateAsync(resource, directoryId, clientId, clientSecret))
+            {
+                Console.WriteLine(await dynamics.GetStringAsync("WhoAmI()"));
+            }
+        }
+    }
+}
