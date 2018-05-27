2f0d830f0510fc59c56b03b3fc2814bfd4b66c46
Alexander <alexander.gurinov@outlook.com>
2018-05-27 12:09:07 +0300

    Basic query support

---
+++ Dyrix/Annotations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dyrix
+{
+    [Flags]
+    public enum Annotations
+    {
+        FormattedValue,
+        AssociatedNavigationProperty = 1,
+        LookupLogicalName = 2,
+        All = FormattedValue & AssociatedNavigationProperty & LookupLogicalName
+    }
+}
--- Dyrix/IDynamicsClient.cs
+++ Dyrix/IDynamicsClient.cs
@@ -1,6 +1,12 @@
-﻿namespace Dyrix
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Dyrix
 {
     public interface IDynamicsClient
     {
+        Task<JObject> QueryAsync(string entitySet, IEnumerable<string> attributes, QueryOptions options = null);
+        Task<int> CountAsyc(string entitySet);
     }
 }
